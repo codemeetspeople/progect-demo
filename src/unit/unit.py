d6213816ea35204134a4c2b2b62b01240130df83
caiman <me@caiman.dev>
2021-01-24 20:06:08 +0200

    [master] Complete Unit class and cover with tests

--- src/unit/unit.py
+++ src/unit/unit.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 def prettify_string(value: str) -> str:
     if not isinstance(value, str):
         raise TypeError(f'value should be of type str: {value}')
@@ -9,7 +12,7 @@ def prettify_string(value: str) -> str:
 
 def check_numeric(value: int):
     value = int(value)
-    if value <= 0:
+    if value < 0:
         raise ValueError(f'value should be positive, got {value} instead')
     return value
 
@@ -19,10 +22,11 @@ class UnitIsDeadException(Exception):
 
 
 class Unit:
-    def __init__(self, name: str, hp: int) -> None:
+    def __init__(self, name: str, hp: int, damage: int) -> None:
         self._name = prettify_string(name)
         self._hp = check_numeric(hp)
         self._maxHP = check_numeric(hp)
+        self._damage = check_numeric(damage)
 
     @property
     def name(self) -> str:
@@ -36,18 +40,48 @@ class Unit:
     def maxHP(self) -> int:
         return self._maxHP
 
+    @property
+    def damage(self) -> int:
+        return self._damage
+
     @hp.setter
     def hp(self, value) -> None:
         self._hp = check_numeric(value)
 
     def __str__(self) -> str:
-        return f'{self.name}: ({self.hp}/{self.maxHP})'
+        return f'{self.name}: ({self.hp}/{self.maxHP}), dmg: {self.damage}'
 
     def __ensure_is_alive(self):
         if self.hp == 0:
             raise UnitIsDeadException()
 
+    def take_damage(self, damage: int) -> None:
+        self.__ensure_is_alive()
+
+        if self.hp - damage < 0:
+            self._hp = 0
+            return
+
+        self.hp -= damage
+
+    def attack(self, enemy: Any) -> None:
+        self.__ensure_is_alive()
+        enemy.take_damage(self.damage)
+        enemy.counter_attack(self)
+
+    def counter_attack(self, enemy: Any) -> None:
+        self.__ensure_is_alive()
+        enemy.take_damage(int(self.damage / 2))
+
 
 if __name__ == '__main__':  # pragma: no cover
-    unit = Unit('SoLdIeR', 100)
-    print(unit)
+    soldier = Unit('Soldier', 100, 20)
+    warrior = Unit('Warrior', 100, 20)
+
+    print(soldier)
+    print(warrior)
+
+    soldier.attack(warrior)
+
+    print(soldier)
+    print(warrior)
--- tests/test_unit/test_unit.py
+++ tests/test_unit/test_unit.py
@@ -1,6 +1,6 @@
 import pytest
 
-from unit.unit import prettify_string, check_numeric
+from unit.unit import prettify_string, check_numeric, Unit, UnitIsDeadException
 
 
 @pytest.mark.parametrize('actual, expected', [
@@ -30,10 +30,83 @@ def test_check_numeric(actual, expected):
 
 @pytest.mark.parametrize('value, exception_type', [
     (-100, ValueError),
-    (0, ValueError),
     ('error', ValueError),
     (dir, TypeError)
 ])
 def test_check_numeric_exception(value, exception_type):
     with pytest.raises(exception_type):
         check_numeric(value)
+
+
+def test_unit_constructor():
+    soldier = Unit('Soldier', 100, 20)
+
+    assert soldier.name == 'Soldier'
+    assert soldier.hp == 100
+    assert soldier.maxHP == 100
+    assert soldier.damage == 20
+
+
+def test_unit_to_string():
+    soldier = Unit('Soldier', 100, 20)
+
+    assert str(soldier) == 'Soldier: (100/100), dmg: 20'
+
+
+def test_unit_setter():
+    soldier = Unit('Soldier', 100, 20)
+
+    assert soldier.hp == 100
+
+    soldier.hp = 50
+    assert soldier.hp == 50
+
+    soldier.hp += 50
+    assert soldier.hp == 100
+
+    soldier.hp -= 90
+    assert soldier.hp == 10
+
+    with pytest.raises(ValueError):
+        soldier.hp = -100
+
+    assert soldier.hp == 10
+
+    with pytest.raises(ValueError):
+        soldier.hp -= 100
+
+    assert soldier.hp == 10
+
+
+def test_unit_attack():
+    soldier = Unit('Soldier', 100, 20)
+    warrior = Unit('Warrior', 100, 20)
+
+    assert soldier.hp == 100
+    assert warrior.hp == 100
+
+    soldier.attack(warrior)
+
+    assert soldier.hp == 90
+    assert warrior.hp == 80
+
+
+def test_unit_attack_exception():
+    soldier = Unit('Soldier', 0, 20)
+    warrior = Unit('Warrior', 100, 20)
+
+    with pytest.raises(UnitIsDeadException):
+        soldier.attack(warrior)
+
+    with pytest.raises(UnitIsDeadException):
+        soldier.counter_attack(warrior)
+
+    with pytest.raises(UnitIsDeadException):
+        soldier.take_damage(warrior.damage)
+
+
+def test_damage_greater_than_hp():
+    soldier = Unit('Soldier', 20, 20)
+
+    soldier.take_damage(30)
+    assert soldier.hp == 0
